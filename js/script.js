4934d79f03c49f0dced8babdc944cff4fdb9e8e9
Pita T.H <Ace-Key@users.noreply.github.com>
2016-04-18 11:19:32 +1200

    update-3-script

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
-	var w1=$("#img_1");
-	var w2=$("#img_2");
-	var w3=$("#img_3");
-	var btn = $(".btn");
-	var w4=$("#img_4");
-	var drop=$(".drop");
+	var w1 = $('#img_1');
+	var w2 = $('#img_2');
+	var w3 = $('#img_3');
+	var w4 = $('#img_4');
+	var cur = $('#w_img');
+	var btn = $('.btn');
+	var drop = $('.drop');
 	
-	var carousel=$(".main-carousel")
-
+	
     
-        carousel.flickity({
-		cellAlign: 'left';
-		wrapAround: true;
-		contain: true;
-		
-	});
-	w1.draggable();
-	w2.draggable();
-	w3.draggable();
-	w4.draggable();
+	/* var carousel=$(".main-carousel"); */
+	
+  	w1.resizable();
+	w2.resizable();
+	w3.resizable();
+	w4.resizable();
+	cur.resizable().draggable();
+	
 	
 	btn.click(function(){
         drop.slideToggle("slow");
 				
     });
-});	
+	
+/* 	carousel.flickity({
+		cellAlign: 'left';
+		wrapAround: true;
+		contain: true;
+		
+	}); */
+
+});		
