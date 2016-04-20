0426a386254ae956ca751c4e2f3c986c3b3c6dc9
Pita T.H <Ace-Key@users.noreply.github.com>
2016-04-20 16:53:51 +1200

    update-api-script-one

--- js/script.js
+++ js/script.js
@@ -6,22 +6,35 @@ $(document).ready(function(){
 	var cur = $('#w_img');
 	var btn = $('.btn');
 	var drop = $('.drop');
-	
-	
-    
+	var cw = $('.cw');    
 	/* var carousel=$(".main-carousel"); */
 	
-  	w1.resizable();
+  	/* w1.resizable();
 	w2.resizable();
 	w3.resizable();
-	w4.resizable();
+	w4.resizable(); */
 	cur.resizable().draggable();
-	
-	
 	btn.click(function(){
         drop.slideToggle("slow");
 				
     });
+	
+	$.simpleWeather({
+    location: 'Wellington, NZ',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<h2 '+weather.code+'>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul style="list-style-type: none; line-height: 1.5;"><li>'+weather.city+', '+weather.country+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      cw.html(html);
+    },
+    error: function(error) {
+      cw.html('<p>'+error+'</p>');
+    }
+  });
 	
 /* 	carousel.flickity({
 		cellAlign: 'left';
