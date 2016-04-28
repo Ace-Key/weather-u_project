709f99dfc7268fe754f6828d5e8b4d10051610b5
Pita T.H <Ace-Key@users.noreply.github.com>
2016-04-29 10:06:25 +1200

    update-4-script

--- js/script.js
+++ js/script.js
@@ -6,18 +6,66 @@ $(document).ready(function(){
 	var cur = $('#w_img');
 	var btn = $('.btn');
 	var drop = $('.drop');
-	var cw = $('.cw');    
-	/* var carousel=$(".main-carousel"); */
+	var cw = $('.cw'); 
+    var ce = $('#cE');
+	var c2 = $('#c2');
 	
-  	/* w1.resizable();
-	w2.resizable();
-	w3.resizable();
-	w4.resizable(); */
+    	
+	/* var carousel=$(".main-carousel"); */
 	cur.resizable().draggable();
+	
 	btn.click(function(){
         drop.slideToggle("slow");
 				
     });
+//light sensor api	
+	window.addEventListener('devicelight', function(event) {
+			
+       var val = event.value;		
+       if (val < 50) {
+         // dark or dim
+		  //ce.innerHTML = ('<p style="text-align: center; font-color: #fffff;">"seems like a dim or dark light"</p>');
+		  ce.html('<p style="color: #ffffff; font-size: 16pt;">Dark'+val+'</p>');
+		} 
+	   else {
+		 // bright or light
+		  //ce.innerHTML = ('<p style="text-align: center; font-color: #fffff;">"seems like a dim or dark light"</p>');
+		  ce.html('<p style="color: #ffffff; font-size: 16pt;">Bright'+val+'</p>');
+		}
+     		
+     console.log(val);
+	 
+	});
+//device orientation api	
+/*   window.addEventListener("devicemotion", handleMotionEvent, true);
+  function handleMotionEvent(event) {
+
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+    var set;
+    if(x>0 || x<0){
+		set = '<p style="font-size: 16pt; color: #ffffff; font-weight: ;">'+x+ 'ms/2''</p>';
+		c2.html(x);
+	}
+    if(y>0 || y<0){
+		set = '<p style="font-size: 16pt; color: #ffffff; font-weight: ;">'+y+ 'ms/2''</p>';
+		c2.html(y);
+	}	
+   if(z>0 || z<0){
+	   set = '<p style="font-size: 16pt; color: #ffffff; font-weight: ;">'+z+ 'ms/2''</p>';
+		c2.html(z);
+	}		
+    
+	console.log('Rotation on the x-axis:'+x+'ms/2');
+	console.log('Rotation on y-axis'+y+'ms/2');
+	console.log('Rotation on z-axis'+z+'ms/2');
+} */
+
+
+
+
+//weather api
 	
 	$.simpleWeather({
     location: 'Wellington, NZ',
@@ -27,6 +75,7 @@ $(document).ready(function(){
       html = '<h2 '+weather.code+'>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul style="list-style-type: none; line-height: 1.5;"><li>'+weather.city+', '+weather.country+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
+	  html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
       cw.html(html);
@@ -34,7 +83,7 @@ $(document).ready(function(){
     error: function(error) {
       cw.html('<p>'+error+'</p>');
     }
-  });
+	});
 	
 /* 	carousel.flickity({
 		cellAlign: 'left';
@@ -43,4 +92,4 @@ $(document).ready(function(){
 		
 	}); */
 
-});		
+});	
