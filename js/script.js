d21f90b7c3a4d54acf613258f4f921e3e742cb68
Pita T.H <Ace-Key@users.noreply.github.com>
2016-05-01 19:10:07 +1200

    update-6-script
    with modal and other updates

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	var cur = $('#w_img');var btn = $('.btn');var drop = $('.drop');var cw = $('#cw');
 	var li1 =$('#li1');	var li2 =$('#li2');
     var geo1 = $('#geo1');var geo2 = $('#geo2');var geo3 = $('#geo3');
-	var o1 = $('#o1'); var o2 = $('#o2');
+	var o1 = $('#o1'); var o2 = $('#o2'); var o3 = $('#o3');
 	var wc1 = $('#wc1');var wc2 = $('#wc2');var wc3 = $('#wc3');var wc4 = $('#wc4');
     var ce = $('#cE');	var c2 = $('#c2');	var c4 = $('#c4');
-	
-    	
+	var s1 = $('#s1'); var s2 = $('#s2'); var s3 = $('#s3'); var s4 = $('#s4');
+    var log = $('#log'); var sign = $('#sign');	var logm = $('#logm'); var signm = $('#signm');
 
 	cur.resizable().draggable();
 //slide function for selectors	
@@ -15,6 +15,15 @@ $(document).ready(function(){
         drop.slideToggle("slow");
 				
     });
+//modal initialisation
+log.click(function() {
+	logm.modal('show');
+});	
+
+sign.click(function() {
+	signm.modal('show');
+});	
+	
 //light sensor api	
 	window.addEventListener('devicelight', function(event) {
 			
@@ -22,13 +31,33 @@ $(document).ready(function(){
        var dis;	   
        if (val < 50) {
          // dark or dim
-		  li1.html("Your in a dim or dark lighting environment:");
-		  li2.html(val + " lux");
+		  li1.html("You're in a dimly lit environment (outside or inside):");
+		  li2.html(val + " lx");
 		} 
-	   else {
-		 // bright or light
-		  li1.html("Your in a bright lighting environment:");
-		  li2.html(val+ " lux");
+	   else if (val < 100) {
+		 // indoors or outside in low light
+		  li1.html("You're either outside (sunset) or inside a building (low light):");
+		  li2.html(val+ " lx");
+		}
+		else if (val < 2000) {
+		 // cloudy
+		  li1.html("Must be cloudy or you're in a brightly lit building:");
+		  li2.html(val+ " lx");
+		}
+		else if (val < 10000) {
+		 // some cloud cover
+		  li1.html("You're probably outside in a partially cloudy day:");
+		  li2.html(val+ " lx");
+		}
+		else if (val < 25000) {
+		 // hazy day
+		  li1.html("You're outside in a sunny day:");
+		  li2.html(val+ " lx");
+		}
+		else if (val < 100000) {
+		 // bright sunny day
+		  li1.html("You're outside in a very bright sunny day:");
+		  li2.html(val+ " lx");
 		}
      		
     	 
@@ -58,11 +87,15 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 	
 //device orientation api	
   
-  function handleMotionEvent(event) {
+function handleMotionEvent(event) {
 
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
+    var x = event.acceleration.x;
+    var y = event.acceleration.y;
+    var z = event.acceleration.z;
+	var x1 = event.accelerationIncludingGravity.x;
+    var y1 = event.accelerationIncludingGravity.y;
+    var z1 = event.accelerationIncludingGravity.z;
+	
  	
     o1.html("Moving Left/Right by: "+x+" ms/2");	
 	o2.html("Moving Forward/Back by: "+y+" ms/2");
@@ -73,9 +106,6 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 	console.log('Rotation on z-axis'+z+'ms/2'); */
 	window.addEventListener("devicemotion", handleMotionEvent, true);
   }
-
-
-
 
 //weather api
 	
@@ -93,7 +123,7 @@ navigator.geolocation.getCurrentPosition(success, error, options);
       wc2.html("Location: " +weather.city+', '+weather.country);
       wc3.html("Current Weather: "+weather.currently);
 	  wc4.html("Wind: " +weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed);
-      //cw.html(html);
+      
     },
     error: function(error) {
       cw.html('<p>'+error+'</p>');
